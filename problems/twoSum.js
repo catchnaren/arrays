13f2f32fa6168a4deaf52374f4c2761f432b2858
Narendrakumar Chandrakumar <naren@Narendrakumars-MacBook-Pro.local>
2022-03-14 01:34:11 +0530

    Solve two sum problem

---
+++ problems/twoSum.js
@@ -0,0 +1,13 @@
+function twoSum(arr, target) {
+  for (let p1 = 0; p1 < arr.length; p1++) {
+    const numberToFind = target - arr[p1];
+    for (let p2 = p1 + 1; p2 < arr.length; p2++) {
+      if (numberToFind === arr[p2]) {
+        return [p1, p2];
+      }
+    }
+  }
+  return null;
+}
+
+console.log(twoSum([1, 4, 6, 9, 2], 11));
