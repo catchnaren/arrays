ad9d2891bc4cca10027403a31938802ee57c69bf
Narendrakumar Chandrakumar <naren@Narendrakumars-MacBook-Pro.local>
2022-03-17 16:46:15 +0530

    Solve singly linkedlist problem

---
+++ problems/linked-lists/reverseLinkedList.js
@@ -0,0 +1,28 @@
+// To reverse the order of nodes in the linked list
+
+const SinglyLinkedList = require('../../data-structures/Singly_Linked_List');
+
+const list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+
+const reverseLinkedList = (head) => {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
+
+// time: O(n)
+// space: O(1)
+
+console.log(reverseLinkedList(list.head));
---
+++ problems/linked-lists/reverseLinkedList2.js
@@ -0,0 +1,51 @@
+/**
+ Given the head of a singly linked list and two integers left and right where left <= right, 
+ reverse the nodes of the list from position left to position right, 
+ and return the reversed list.
+ */
+
+const SinglyLinkedList = require('../../data-structures/Singly_Linked_List');
+
+const list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+
+const reverseBetween = (head, m, n) => {
+  let currentPos = 1,
+    currentNode = head,
+    start = head;
+
+  while (currentPos < m) {
+    start = currentNode;
+    currentNode = currentNode.next;
+    currentPos++;
+  }
+
+  let newList = null,
+    tail = currentNode;
+
+  while (currentPos >= m && currentPos <= n) {
+    const next = currentNode.next;
+    currentNode.next = newList;
+    newList = currentNode;
+    currentNode = next;
+    currentPos++;
+  }
+
+  start.next = newList;
+  tail.next = currentNode;
+
+  if (m > 1) {
+    return head;
+  } else {
+    return newList;
+  }
+};
+
+// time: O(n)
+// space: O(1)
+
+console.log(reverseBetween(list.head, 1, 4));
